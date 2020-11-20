ba29b174c53d9116f0256575dad80ab1029292cd
abhi717 <59393914+abhi717@users.noreply.github.com>
2020-11-20 15:07:26 +0530

    Add files via upload
    VitaApp

---
+++ VitaApp.java
@@ -0,0 +1,15 @@
+package course;
+public class VitaApp
+{
+	public static void main(String args[])
+	{
+		Dac dac = new Dac();
+		PreDac pre = new PreDac();
+		Basic basic = new Basic();
+		dac.term1();
+		dac.term2();
+		pre.cet();
+		basic.finalTest();
+	}
+
+}
